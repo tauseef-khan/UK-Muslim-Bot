97d6e653d6c0d06859fe84da2c185c089ac1e041
tauseef-khan <tdakhan@gmail.com>
2018-02-11 21:50:21 +0000

    initial commit

---
+++ bot.py
@@ -0,0 +1,88 @@
+import logging
+import os
+import sys
+
+import locationfeature
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import KeyboardButton, ReplyKeyboardMarkup
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+def echo(bot, update):
+    update.effective_message.reply_text(update.effective_message.text)
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+def location(bot, update):
+
+    bot.send_message(chat_id=update.message.chat_id, text="Make sure your location is turned on. For optimal results, use 'High Accuracy' mode.")
+    
+    # creating location button object
+    location_keyboard = KeyboardButton(text="send_location",  request_location=True)
+   
+    # creating keyboard object
+    custom_keyboard = [[location_keyboard]]
+    
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard) 
+    
+    update.message.reply_text("Would you mind sharing your location with me?", reply_markup=reply_markup)
+
+def getMosques(bot, update):
+
+    lat = update.message.location.latitude
+    lon = update.message.location.longitude
+
+    closestMosques = locationfeature.getClosestMosques(lat, lon)
+
+    count = 1
+
+    for mosque in closestMosques:
+        message_text = str(count) + '\n' + mosque
+        bot.send_message(chat_id=update.message.chat_id, text=message_text, parse_mode="Markdown")
+        count = count + 1
+
+# method for debuggung
+def log(message):
+
+    print(message)
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+
+    # Set these variable to the appropriate values
+    TOKEN = os.environ["TELEGRAM_TOKEN"]
+    NAME = os.environ["APP_NAME"]
+
+    # Port is given by Heroku
+    PORT = os.environ.get('PORT')
+
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Set up the Updater
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+
+    # ADD HANDLERS
+    # start command
+    dp.add_handler(CommandHandler('start', start))
+
+    # location command
+    dp.add_handler(CommandHandler('location', location))
+    dp.add_handler(MessageHandler(Filters.location, getMosques))
+
+    #dp.add_handler(MessageHandler(Filters.text, echo))
+    #dp.add_handler(error)
+
+    # Start the webhook
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    updater.idle()
---
+++ locationfeature.py
@@ -0,0 +1,97 @@
+import pandas as pd
+from math import sin, cos, sqrt, atan2, radians
+
+# I believe the mosque data is read everytime a method in this file is used
+filepath = "MosquesJan18Extended_interpreted.csv"
+mosque_df = pd.read_csv(filepath, encoding="latin1")
+
+# method to calculate distance between two co-ordinates using Haversine formula
+# mehod poached/borrowed from this StackOverflow question...
+# ...https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude
+def haversine(lon1, lat1, lon2, lat2):
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    
+    return distance
+
+def getClosestMosques(userLat, userLon):
+
+	closestMasjidsData = []
+
+	originalUserLat = userLat
+	originalUserLon = userLon
+
+	userLat = radians(userLat)
+	userLon = radians(userLon)
+
+	# create dataframe to store distances from user's location
+	columns = ['Distance']
+	masjid_distance = pd.DataFrame(columns=columns)
+
+
+	# calculate distances to each masjid in 'mosque_df'
+	for index, row in mosque_df.iterrows():
+	    
+	    #extract masjid lat and lon
+	    mosqueLat = radians(row['Latitude'])
+	    mosqueLon = radians(row['Longitude'])
+	    
+	    # calculate distance to masjid
+	    distance = haversine(mosqueLon, mosqueLat, userLon, userLat)
+	    
+	    # append distance to 'masjid_distance' dataframe
+	    masjid_distance = masjid_distance.append({'Distance':distance}, ignore_index=True)
+
+
+	# sort distance into ascending order
+	masjid_distance_sorted = masjid_distance.sort_values(['Distance'], ascending=True)
+
+	# get top 5 smallest distance
+	masjid_distance_sorted_indexes = masjid_distance_sorted.index.values[:5]
+
+
+	# store masjid details for user to see
+	count = 0
+	for index in masjid_distance_sorted_indexes:
+
+		distance = "*Distance: *" + str(masjid_distance_sorted.values[count].round(3)) + " km"
+		count = count + 1
+
+		masjidLat = mosque_df.iloc[index]['Latitude']
+		masjidLon = mosque_df.iloc[index]['Longitude']
+
+		masjidName = mosque_df.iloc[index]['[Key]_Address_Line_1']
+		masjidName = "*Name & Addr: *" + masjidName[masjidName.find("]")+1:]
+
+		masjidCapacity = "*Capacity: *" + str(mosque_df.iloc[index]['Capacity'])
+		masjidWomen = "*Women facilities: *" + mosque_df.iloc[index]['Women facility']
+		masjidUsage = "*Usage: *" + mosque_df.iloc[index]['Masjid usage']
+		masjidMultifaith = "*Multi-faith: *" + mosque_df.iloc[index]['Multi-faith']
+		masjidTheme = "*Theme: *" + mosque_df.iloc[index]['Theme']
+		masjidManagement = "*Management: *" + mosque_df.iloc[index]['Management']
+		mapUrl = generateMapUrl(originalUserLat, originalUserLon, masjidLat, masjidLon)
+
+		message_text = masjidName + '\n' + masjidCapacity + '\n' + masjidWomen + '\n' + masjidUsage + '\n' + masjidMultifaith + '\n' + masjidTheme + '\n' + masjidManagement + '\n' + distance + "\n" + mapUrl
+
+		closestMasjidsData.append(message_text)
+
+	return closestMasjidsData
+
+def generateMapUrl(userLat, userLon, masjidLat, masjidLon):
+
+	generatedUrl = "(" + "https://www.google.com/maps/dir/?api=1&origin=" + str(userLat) + "," + str(userLon) + "&destination=" + str(masjidLat) + "," + str(masjidLon) + ")"
+
+	return "[Get Directions]" + generatedUrl
