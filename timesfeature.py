d348a509e7cb85c32ec96a22f331b721aecd7595
tauseef-khan <tdakhan@gmail.com>
2018-02-25 00:20:43 +0000

    fixed file not found error

--- timesfeature.py
+++ timesfeature.py
@@ -95,7 +95,7 @@ def getCellNumber(day):
 
 # get prayer times given the cellRange
 def getPrayerTimes(filename, cellNumber):
-    filepath = "times\\elm-" + filename + ".csv"
+    filepath = "times\elm-" + filename + ".csv"
     times_data = pd.read_csv(filepath, encoding = "latin1")
     
     times_data = times_data.iloc[[cellNumber]]
