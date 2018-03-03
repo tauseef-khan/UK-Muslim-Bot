64598025eefcdafd6677e2dcf2e04b73cbf9f5de
tauseef-khan <tdakhan@gmail.com>
2018-03-03 15:51:28 +0000

    added functionality to enable the commands list feature to be a thing

---
+++ commandslistfeature.py
@@ -0,0 +1,12 @@
+def readCommands():
+
+	filename = "commandslist.txt"
+	commandsText = open(filename, "r")
+
+	lines = commandsText.readlines()
+
+	message = ""
+	for line in lines:
+    	message += line
+
+    return message
