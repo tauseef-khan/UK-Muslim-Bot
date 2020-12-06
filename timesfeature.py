4749a6489f8448737b3cb106d934493e11d37b16
tauseef-khan <tdakhan@gmail.com>
2020-12-06 12:28:45 +0000

    preparing for new year with 2021 prayer times data

--- timesfeature.py
+++ timesfeature.py
@@ -151,7 +151,7 @@ def construct_schedule(date):
 
     # GLOBAL VARIABLES
     # to be changed at the start of a new year
-    year = 2020
+    year = 2021
     strYear = str(year)[2:4]
 
     #now = None
